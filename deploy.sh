9f606937bf534dff1e32f515d3c6d5e3487da5a4
SMoni <SMoni@users.noreply.github.com>
2020-12-11 08:41:38 +0100

    removed comments

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,9 @@
 # abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
