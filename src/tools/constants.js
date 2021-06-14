b4a0011b455120dff79f9a2752df14b0cdbd49dd
SMoni <steffen.monarth@ingenieur.io>
2021-06-14 08:53:37 +0200

    added dominant phrygian... for metal heads :)

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -3,18 +3,19 @@ const scales = {
 }
 
 const modes = {
-    major:           [ 2, 2, 1, 2, 2, 2, 1 ], // ionian
-    majorPentatonic: [ 2, 2, 3, 2 ], 
-    minor:           [ 2, 1, 2, 2, 1, 2, 2 ], // aeolian
-    minorPentatonic: [ 3, 2, 2, 3 ], 
-    minorHarmonic:   [ 2, 1, 2, 2, 1, 3, 1 ],
-    ionian:          [ 2, 2, 1, 2, 2, 2, 1 ],
-    dorian:          [ 2, 1, 2, 2, 2, 1, 2 ],
-    phrygian:        [ 1, 2, 2, 2, 1, 2, 2 ],
-    lydian:          [ 2, 2, 2, 1, 2, 2, 1 ],
-    mixolydian:      [ 2, 2, 1, 2, 2, 1, 2 ],
-    aeolian:         [ 2, 1, 2, 2, 1, 2, 2 ],
-    locrian:         [ 1, 2, 2, 1, 2, 2, 2 ],
+    major:            [ 2, 2, 1, 2, 2, 2, 1 ], // ionian
+    majorPentatonic:  [ 2, 2, 3, 2 ], 
+    minor:            [ 2, 1, 2, 2, 1, 2, 2 ], // aeolian
+    minorPentatonic:  [ 3, 2, 2, 3 ], 
+    minorHarmonic:    [ 2, 1, 2, 2, 1, 3, 1 ],
+    ionian:           [ 2, 2, 1, 2, 2, 2, 1 ],
+    dorian:           [ 2, 1, 2, 2, 2, 1, 2 ],
+    phrygian:         [ 1, 2, 2, 2, 1, 2, 2 ],
+    phrygianDominant: [ 1, 3, 1, 2, 1, 2, 2 ],    
+    lydian:           [ 2, 2, 2, 1, 2, 2, 1 ],
+    mixolydian:       [ 2, 2, 1, 2, 2, 1, 2 ],
+    aeolian:          [ 2, 1, 2, 2, 1, 2, 2 ],
+    locrian:          [ 1, 2, 2, 1, 2, 2, 2 ],
 }
 
 const chords = {
@@ -24,7 +25,6 @@ const chords = {
     minorSeventh: [ 3, 4, 3 ],
     sus2:         [ 2, 5 ],
     sus4:         [ 5, 2 ],    
-
 }
 
 export { 
